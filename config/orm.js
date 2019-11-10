dc4d88d455696b72a77f6bdc741ea84f8b72c49a
JackRyan1989 <jack.jackryan@gmail.com>
2019-11-09 21:57:25 -0500

    Completed orm. Needs to be tested.

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,37 @@
 const connection = require('./connection');
 
+//Object for all orm functions:
+const orm = {
+    selectAll: function (table, cb) {
+        let queryStr = "SELECT * FROM" + table + ';';
+        connection.query(queryStr, function (err, result) {
+            if (err) {
+                result.status(500).end();
+            }
+            cb(result);
+        });
+    },
+    selectOne: function (table, colId, cb) {
+        let queryStr = "SELECT * FROM" + table + "WHERE id = " + colId;
+        connection.query(queryStr, function (err, result) {
+            if (err) {
+                res.status(500).end();
+            }
+            cb(result);
+        });
+    },
+    updateOne: function (table, bName, dev, colId, cb) {
+        let queryStr = "UPDATE " + table + " SET burgerName = " + bName + ", devoured = " + dev + "WHERE id = " + colId;
+        connection.query(queryStr, function(err, result){
+            if (err) {
+                res.status(500).end();
+            }
+            cb(result);
+        });
+    }
+};
 
+
+
+
+module.exports = orm;
