f8378b042bbb483f802493f9b671b18a0f1f6843
JackRyan1989 <jack.jackryan@gmail.com>
2019-11-09 21:35:51 -0500

    Connection config file done.

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+
+connection = mysql.createConnection({
+    host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "dM*4V7c^pK",
+  database: "burgers_db"
+});
+
+connection.connect(function(err){
+    if (err) {
+        console.log(err);
+    }
+});
+
+module.exports = connection;
+
+
